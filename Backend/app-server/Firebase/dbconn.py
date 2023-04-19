4c6b0d35c0c12e0c9388025108a605e14cc2ef76
Arjun Naik <arjunsatishnaik@gmail.com>
2023-04-18 19:27:02 -0700

    added crud functions

--- Backend/app-server/Firebase/dbconn.py
+++ Backend/app-server/Firebase/dbconn.py
@@ -9,14 +9,36 @@ default_app = initialize_app(cred)
 
 db = firestore.client()
 
-userdb = db.collection('Users')
-
-def create(id: str):
+def create(collection, new):
 	try:
-		userdb.add(
-			{"created":dt.now(), "email": "e123@uw.edu", "username":"D3"}
-		)
+		coll = db.collection(collection)
+		coll.add(new)
+		return ("Success", True, 200)
+	except Exception as e:
+		return (f"Failed: {e}", False, 000)
+	
+def read(collection):
+	try:
+		coll = db.collection(collection)
+		vals = {}
+		for i in coll.stream():
+			vals[i] = i.to_dict()
+		return (vals, True, 200)
 	except Exception as e:
 		return (f"Failed: {e}", False, 000)
 
-create('Users')
+def update(collection, id, new):
+	try:
+		coll = db.collection(collection)
+		coll.document(id).set(new)
+		return ("Success", True, 200)
+	except Exception as e:
+		return (f"Failed: {e}", False, 000)
+
+def delete(collection, id):
+	try:
+		coll = db.collection(collection)
+		coll.document(id).delete()
+		return ("Success", True, 200)
+	except Exception as e:
+		return (f"Failed: {e}", False, 000)
