56769e221dcc543a2886c53ad4b7c7e154847791
Arjun Naik <arjunsatishnaik@gmail.com>
2023-05-02 15:07:28 -0700

    added files

---
+++ Backend/app-server/Firebase/main.py
@@ -0,0 +1,16 @@
+from endpoints import *
+
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
