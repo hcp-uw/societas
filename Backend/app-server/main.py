ce758406f47ec11d359f600903a2a46a42db7a92
Arjun Naik <arjunsatishnaik@gmail.com>
2023-05-13 16:24:28 -0700

    removed conflicting import

--- Backend/app-server/main.py
+++ Backend/app-server/main.py
@@ -1,9 +1,8 @@
-from endpoints import *
+#from endpoints import *
 
 import os
 
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
