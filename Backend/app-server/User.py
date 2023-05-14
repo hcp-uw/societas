013b8e43852dc09d76d4d4605744fb3b40edee33
Arjun Naik <arjunsatishnaik@gmail.com>
2023-05-13 18:16:11 -0700

    changed pwd hashing

--- Backend/app-server/User.py
+++ Backend/app-server/User.py
@@ -39,7 +39,7 @@ def hash2(password, salt):
 def comparePWD(password, user):
     stored_hash = (user.password_hash)
     salt = user.password_salt
-    password_bytes = base64.b64encode(password)#password.encode('utf-8')
+    password_bytes = password.encode('utf-8')
     generated_hash = scrypt.hash(password_bytes, salt=salt, N=2**14, r=8, p=1, dklen=64)
     return stored_hash == generated_hash
 
