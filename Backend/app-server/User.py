7c09a81964512806fe0601af98dd74a5e04763c9
Arjun Naik <arjunsatishnaik@gmail.com>
2023-05-13 17:33:28 -0700

    added testing

--- Backend/app-server/User.py
+++ Backend/app-server/User.py
@@ -6,14 +6,13 @@ from Firebase.dbconn import *
 from core import *
 ROUTE = '/user/'
 
-def hashpwd(password):
+def hashpwd(password, salt):
     # Convert base64-encoded parameters to byte arrays
     signer_key = base64.b64decode("wawILo40NkG69NfUuESYubil5/dQB4vccZFhqAz+SGHg6utWCOQyGR7qHxB6k8VgIRlKoIVFXcgtlrBztqLlWA==")
     salt_separator = base64.b64decode("Bw==")
     # Generate random salt value
-    salt = os.urandom(16)
     # Concatenate salt separator and salt value
-    salted = salt_separator
+    salted = salt_separator + salt
     # Hash password with SCRYPT algorithm
     kdf = Scrypt(
         salt=salted,
@@ -26,7 +25,7 @@ def hashpwd(password):
     key = kdf.derive(password.encode())
     
     # Store salt and hashed password in database
-    stored_password = base64.b64encode(key).decode()
+    stored_password = base64.b64encode(salt + key).decode()
     return stored_password
 
 class Auth:
@@ -40,7 +39,7 @@ class Auth:
                 if user.password_hash == hashpwd(pwd):
                     setcurr(user.uid)
                     return str(Status(False, f'Successfully logged in {email}'))
-                return str(Status(False, f'Password is incorrect. actual: {user.password_hash}. passed: {hashpwd(pwd)}'))
+                return str(Status(False, f'Password is incorrect. actual: {user.password_hash}. passed: {hashpwd(pwd, user.password_salt)}'))
         return str(Status(False, f'User with email {email} does not exist.'))
 
         
