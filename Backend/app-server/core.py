28ea7c92e99a019b38c76d5bb4ddf2cfe02dbfde
Arjun Naik <arjunsatishnaik@gmail.com>
2023-10-11 13:07:16 -0700

    changed json separator

--- Backend/app-server/core.py
+++ Backend/app-server/core.py
@@ -8,7 +8,7 @@ class Status:
         self.message = message
     
     def __str__(self):
-        return json.dumps({"success":self.success,"message":self.message}, indent=, separators =(", ", " : "),)
+        return json.dumps({"success":self.success,"message":self.message}, indent=4, separators =(", ", " : "),)
     
 
 getcurr = lambda: session.get('curr')
