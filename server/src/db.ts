84f11c3d5609494efd878fb204dddc0bab336676
Santiago Garcia <sagaan@outlook.com>
2024-04-13 12:53:41 -0700

    foratting

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,12 @@
 // this is known a singleton that way there is always just one connection to the database
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+    prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error", "warn"],
-  });
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query', 'error', 'warn'],
+    });
