6df74f84367fee6f9d534a55a8abe49110c32197
Hansel Lee <59949027+hlee131@users.noreply.github.com>
2024-05-21 18:44:02 -0700

    Add VITE prefix to env variable (#70)
    * add env variable for backend url
    
    * actually change the backend url
    
    ---------
    
    Co-authored-by: Hansel Lee <mr.hansel.lee@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,7 @@ export default function App({ queryClient }: { queryClient: QueryClient }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: import.meta.env?.BACKEND_URL ?? 'http://localhost:3001',
+          url: import.meta.env?.VITE_BACKEND_URL ?? 'http://localhost:3001',
           // You can pass any HTTP headers you wish here
           async headers() {
             const token = await getToken();
